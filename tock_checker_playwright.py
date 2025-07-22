3c7ae1241c23b9229041c5a6dca6ef283b593509
Ziyi Jiang <zj644@nyu.edu>
2025-07-22 00:04:15 -0700

    fix availably function 2

--- tock_checker_playwright.py
+++ tock_checker_playwright.py
@@ -33,7 +33,7 @@ async def check_page(playwright):
     browser = await playwright.chromium.launch(headless=True)
     page = await browser.new_page()
     try:
-        await page.goto(TOCK_URL, wait_until='domcontentloaded', timeout=60000)
+        await page.goto(TEST_URL, wait_until='domcontentloaded', timeout=60000)
         # wait for the calendar to appear
         await page.wait_for_selector('button.ConsumerCalendar-day')
 
